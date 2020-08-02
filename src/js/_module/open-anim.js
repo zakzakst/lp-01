12b045bb26d1046e543dd49acef30b730b39ade6
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-08-02 20:47:42 +0900

    ページローダーの作成

--- src/js/_module/open-anim.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-import { gsap } from "gsap";
-
-export function openAnim() {
-  setTimeout(() => {
-    $('.hero__ttl, .hero__subttl').addClass('is-animated');
-  }, 1000);
-}
--- src/js/script.js
+++ src/js/script.js
@@ -2,15 +2,13 @@ import $ from 'jquery';
 import { goTop } from './_module/go-top';
 import { headerScripts } from './_module/header';
 import { pageLoader } from './_module/page-loader';
-import { openAnim } from './_module/open-anim';
 import { scrollAnim } from './_module/scroll-anim';
 import { formValidate } from './_module/form-validate';
 
 $(function() {
   goTop();
   headerScripts();
-  // pageLoader();
-  openAnim();
+  pageLoader();
   scrollAnim();
   formValidate();
 });
